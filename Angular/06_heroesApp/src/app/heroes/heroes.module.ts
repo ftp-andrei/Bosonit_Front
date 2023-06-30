f1b0797888c06f5106b3cbd67f145938411c4998
Andrei Alexandru Miu <andrei.miu@bosonit.com>
2023-06-30 12:34:51 +0200

    add 06,07,08,09,10,11 full

--- Angular/06_heroesApp/src/app/heroes/heroes.module.ts
+++ Angular/06_heroesApp/src/app/heroes/heroes.module.ts
@@ -8,6 +8,10 @@ import { ListPageComponent } from './pages/list-page/list-page.component';
 import { NewPageComponent } from './pages/new-page/new-page.component';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
 import { MaterialModule } from '../material/material.module';
+import { CardComponent } from './components/card/card.component';
+import { HeroImagePipe } from './pipes/hero-image.pipe';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ConfirmDialogComponent } from './components/confirm-dialog/confirm-dialog.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +20,15 @@ import { MaterialModule } from '../material/material.module';
     ListPageComponent,
     NewPageComponent,
     SearchPageComponent,
+    CardComponent,
+    HeroImagePipe,
+    ConfirmDialogComponent,
   ],
-  imports: [CommonModule, HeroesRoutingModule, MaterialModule],
+  imports: [
+    CommonModule,
+    HeroesRoutingModule,
+    MaterialModule,
+    ReactiveFormsModule,
+  ],
 })
 export class HeroesModule {}
--- Angular/06_heroesApp/src/app/heroes/pages/hero-page/hero-page.component.ts
+++ Angular/06_heroesApp/src/app/heroes/pages/hero-page/hero-page.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs';
+import { Hero } from '../../interfaces/hero.interfaces';
 
 @Component({
   selector: 'app-hero-page',
   templateUrl: './hero-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class HeroPageComponent {
+export class HeroPageComponent implements OnInit {
+  public hero?: Hero;
 
+  constructor(
+    private heroesService: HeroesService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.activatedRoute.params
+      .pipe(switchMap(({ id }) => this.heroesService.getHeroById(id)))
+      .subscribe((heroe) => {
+        if (!heroe) return this.router.navigate(['/heroes.list']);
+
+        this.hero = heroe;
+        console.log(this.hero);
+        return;
+      });
+  }
+
+  goBack(): void {
+    this.router.navigateByUrl('heroes/list');
+  }
 }
